fc4ea94c0a714a8cda9fefc4d6e883a368a6f687
hayden <hayden@hayzak.com>
2017-02-07 19:53:45 -0600

    Added code from 2/7 lecture

--- server.js
+++ server.js
@@ -7,8 +7,10 @@
  */
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var port = 3000;
 
+var config = JSON.parse(fs.readFileSync('config.json'));
 var stylesheet = fs.readFileSync('gallery.css');
 
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
@@ -30,12 +32,20 @@ function namesToTags(fileNames) {
 function buildGallery(imageTags) {
   var html = "<!doctype html>";
       html += "<head>";
-      html +=   "<title>Gallery</title>";
+      html +=   "<title>" + config.title + "</title>";
       html +=   "<link href= 'gallery.css' rel='stylesheet' type='text/css'>"
       html += "</head>"
       html += '<body>';
-      html += ' <h1>Gallery</h1>';
+      html += ' <h1>'+ config.title + '</h1>';
+      html += ' <form action="">';
+      html += '   <input type="text" name = "title">';
+      html += '   <input type="submit" value ="change gallery title">';
+      html += ' </form>';
       html += namesToTags(imageTags).join('');
+      html += 'form action="" method="POST" enctype="multipart/form-data">';
+      html += ' <input type="file" name="image">';
+      html += ' <input type="submit" value="Upload Image">';
+      html += '</form>';
       html += ' <h1>Hello</h1> the time is ' + Date.now();
       html += '</body>';
       return html;
@@ -56,6 +66,29 @@ function serveGallery(req, res) {
     });
 }
 
+function uploadImage(req, res){
+  var body = '';
+  req.on('error', function(){
+    res.statusCode = 500;
+    res.end();
+
+  });
+  req.on('data', function(data){
+    body += data;
+  });
+  req.on('end', function(){
+    fs.writeFile('filename', data, function(){
+      if(err){
+        console.error(err);
+        res.statusCode = 500;
+        res.end();
+        return;
+      }
+      
+    });
+  });
+}
+
 function serveImage(filename, req, res)
 {
   var body = fs.readFile(`images/${filename}`, function(err, body){
@@ -72,11 +105,34 @@ function serveImage(filename, req, res)
 }
 
 var server = http.createServer((req, res) => {
-  switch(req.url) {
+
+  //at most, the url should have two parts-
+  //a resource and a query string separated by a ?
+  var urlParts = url.parse(req.url);
+
+
+  var resource = url[0];
+  var queryString =url[1];
+
+  if(urlParts.query){
+    var matches = /title=(.+)(&|$)/.exec(urlParts.query);
+    if(matches && matches[1]){
+      config.title = decodeURIComponent(matches[1]);
+      fs.writeFile('config.json', JSON.stringify(config));
+
+    }
+  }
+
+  switch(urlParts.pathname) {
     case'/':
     case '/gallery':
-    console.log('hit gallery');
-      serveGallery(req, res);
+      if(req.method == 'GET'){
+        console.log('hit gallery');
+        serveGallery(req, res);
+      }else if (req.method == 'POST') {
+        uploadPicture
+      }
+
     break;
     case '/gallery.css':
       res.setHeader('Content-Type', 'text/css');
